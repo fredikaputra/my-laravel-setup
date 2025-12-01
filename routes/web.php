30a5e7ab9724f4a6438bba9a1f5689e68c25a135
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2025-12-01 21:59:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => response()->json('Laravel API'))->name('home');
+Route::get('/', fn () => response()->json('Laravel API'));
 Route::get('/login', fn () => response()->json(['message' => 'The GET method is not supported for route login. Supported method: POST.'], 405))->name('login');
