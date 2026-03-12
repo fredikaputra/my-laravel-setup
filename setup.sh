b5918c98648f2183453aaf2e8a917e3a87efd55d
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2026-03-12 11:41:31 +0700

    WIP

--- setup.sh
+++ setup.sh
@@ -1,124 +1,55 @@
 #!/bin/bash
 
-sed -i 's/php artisan serve/php artisan schedule:work/g' composer.json
-sed -i 's/\\"bun run dev\\" --names=server,queue,logs,vite/\\"php artisan horizon\\" --names=schedule,queue,logs,horizon/g' composer.json
-sed -i '/npm/d' composer.json
-npx prettier --write composer.json
+cat << 'EOF' >> ~/.bash_aliases
+alias composer-latest='composer require $(composer show -s --format=json | jq -r ".requires | keys[]" | grep -v "^php$")'
+alias composer-latest-dev='composer require --dev $(composer show -s --format=json | jq -r ".devRequires | keys[]")'
+EOF
 
-composer require fredikaputra/activity-logger \
-                fredikaputra/socialite-boilerplate \
-                fredikaputra/async-logger \
-                laravel/socialite \
-                laravel/fortify \
-                laravel/octane \
-                laravel/horizon \
-                laravel/scout \
-                dedoc/scramble \
-                meilisearch/meilisearch-php
-composer require laravel/telescope laravel/sail laravel/pulse spatie/laravel-web-tinker --dev -n
-composer update
-php artisan telescope:install
-php artisan install:api --passport
-php artisan fortify:install
-php artisan sail:publish
-php artisan horizon:install
-php artisan vendor:publish --tag=ai-routes
+source ~/.bash_aliases
 
+clear
+
+CURRENT_FOLDER=$(basename "$PWD")
+
+mv .git ..
+
+cd ..
+
+rm -rf "$CURRENT_FOLDER"
+
+git clone https://github.com/nunomaduro/laravel-starter-kit-inertia-react "$CURRENT_FOLDER"
+
+rm -rf "$CURRENT_FOLDER/.git"
+
+mv .git "$CURRENT_FOLDER"
+
+cd "$CURRENT_FOLDER"
+
+read -p 'Enter...'
 git add .
 
-MIG_DIR="database/migrations"
-PREFIX="0001_01_01_"
+composer-latest
+composer-latest-dev
+bun update --latest
+echo '{}' > .oxlintrc.json
 
-rm database/migrations/0001_01_01_000001_create_cache_table.php
-mv database/migrations/0001_01_01_000000_create_users_table.php database/migrations/0001_01_01_000001_create_users_table.php
-
-last_index=$(ls "$MIG_DIR"/"$PREFIX"*.php 2>/dev/null | sed -E "s/.*$PREFIX([0-9]+)_.*/\1/" | sort -rn | head -1)
-
-if [ -z "$last_index" ]; then
-    last_index=-1
-else
-    last_index=$((10#$last_index))
-fi
-
-for file in $(ls "$MIG_DIR"/*.php | grep -v "$PREFIX" | sort); do
-    last_index=$((last_index + 1))
-    
-    next_num=$(printf "%06d" "$last_index")
-    
-    filename=$(basename "$file")
-    
-    base_name=$(echo "$filename" | sed -E 's/^[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{6}_//')
-    
-    new_name="${PREFIX}${next_num}_${base_name}"
-    
-    mv "$file" "$MIG_DIR/$new_name"
-done
-
-read -p 'Enter to continue...'
+read -p 'Enter...'
 git add .
 
-TARGET_DIR="database/migrations"
+echo 'Try run composer lint and test'
 
-if [ ! -d "$TARGET_DIR" ]; then
-  echo "Error: Directory $TARGET_DIR does not exist."
-  exit 1
-fi
-
-find "$TARGET_DIR" -type f -name "*.php" -exec sed -i "s/foreignId('user_id')/foreignUuid('user_id')/g" {} +
-
-read -p 'Enter to continue...'
-git add .
-
-rm -r config
-cp -r my-laravel-setup/src/. .
 cp .env.example .env
-mv docker/8.4/* .devcontainer
-mv docker/mysql/create-testing-database.sh .devcontainer
 
-read -p 'Enter to continue...'
+APP_NAME_VALUE=$(grep '^APP_NAME=' .env | cut -d '=' -f2- | tr -d '"' | tr -d "'")
+
+sed -i "s|\${APP_NAME}|$APP_NAME_VALUE|g" .env
+sed -i "s|^APP_URL=.*|APP_URL=https://9002-$WEB_HOST|" .env
+sed -i "s|^ASSET_URL=.*|ASSET_URL=https://9002-$WEB_HOST|" .env
+sed -i "s|^HMR_HOST=.*|HMR_HOST=5173-$WEB_HOST|" .env
+
+> public/hot
+
+php artisan key:generate
+
+read -p 'Enter...'
 git add .
-
-composer lint
-php artisan migrate:fresh
-
-sed -i '/^\/public\/storage/a\/public\/vendor' .gitignore
-
-node -e "
-const fs = require('fs');
-const file = 'composer.json';
-const json = JSON.parse(fs.readFileSync(file));
-
-json.extra = json.extra || {};
-json.extra.laravel = json.extra.laravel || {};
-json.extra.laravel['dont-discover'] = ['laravel/telescope'];
-
-fs.writeFileSync(file, JSON.stringify(json, null, 4));
-"
-echo >> composer.json
-
-rm -rf .cursor \
-        .idx \
-        .junie \
-        art \
-        docker \
-        public/favicon.ico \
-        resources \
-        tests/Browser \
-        .mcp.json \
-        bun.lock \
-        package.json \
-        boost.json \
-        AGENTS.md \
-        CLAUDE.md \
-        README.md \
-        vite.config.js \
-        app/Actions/.gitkeep \
-        app/Providers/HorizonServiceProvider.php \
-        app/Providers/TelescopeServiceProvider.php \
-        my-laravel-setup
-
-read -p 'Enter to continue...'
-git add .
-
-php artisan sail:install --with=redis,meilisearch,mailpit
-echo 'APP_PORT=8000' >> .env
