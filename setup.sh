3b7649c8e1f549ce897a3aea6a11b601b4733621
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2026-03-14 15:53:12 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -1,43 +1,26 @@
 #!/bin/bash
 
-cat << 'EOF' >> ~/.bash_aliases
-alias composer-latest='composer require $(composer show -s --format=json | jq -r ".requires | keys[]" | grep -v "^php$")'
-alias composer-latest-dev='composer require --dev $(composer show -s --format=json | jq -r ".devRequires | keys[]")'
-EOF
-
-source ~/.bash_aliases
-
 clear
 
 CURRENT_FOLDER=$(basename "$PWD")
 
 mv .git ..
-
 cd ..
-
 rm -rf "$CURRENT_FOLDER"
-
 git clone https://github.com/nunomaduro/laravel-starter-kit-inertia-react "$CURRENT_FOLDER"
-
 rm -rf "$CURRENT_FOLDER/.git"
-
 mv .git "$CURRENT_FOLDER"
-
 cd "$CURRENT_FOLDER"
 
-read -p 'Enter...'
+read -p 'Press Enter to update dependencies...'
 git add .
-
-composer-latest
-composer-latest-dev
+composer require $(composer show -s --format=json | jq -r ".requires | keys[]" | grep -v "^php$")
+composer require --dev $(composer show -s --format=json | jq -r ".devRequires | keys[]")
 bun update --latest
 echo '{}' > .oxlintrc.json
 
-read -p 'Enter...'
+read -p 'Press Enter to configure .env...'
 git add .
-
-echo 'Try run composer lint and test'
-
 cp .env.example .env
 
 APP_NAME_VALUE=$(grep '^APP_NAME=' .env | cut -d '=' -f2- | tr -d '"' | tr -d "'")
@@ -48,8 +31,7 @@ sed -i "s|^ASSET_URL=.*|ASSET_URL=https://9002-$WEB_HOST|" .env
 sed -i "s|^HMR_HOST=.*|HMR_HOST=5173-$WEB_HOST|" .env
 
 > public/hot
-
 php artisan key:generate
 
-read -p 'Enter...'
+read -p 'Finalizing...'
 git add .
